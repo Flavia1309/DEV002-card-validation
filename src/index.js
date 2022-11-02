6ebd5793c59676671ac0106e9eb61f746d7cae4a
Flavia Uribe <flavia.uribejeldes@gmail.comgit config --global user.email flavia.uribejeldes@gmail.com>
2022-11-02 13:34:48 -0300

    creando codigo luhn, para aceptar solo digitos del 0-9 y sin espacios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import validator from './validator.js';
+//validar tarjeta de crédito
+function fValidar tarjeta(){
+var opt = $("lstTipoTarjeta option:selected").val()
+codigo = $("numero_tarjeta").val().replace('-','')
+}
+
 
 console.log(validator);
