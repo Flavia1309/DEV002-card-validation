e57675443468d8cb8853d01796907e7ee38d557e
Flavia Uribe <flavia.uribejeldes@gmail.comgit config --global user.email flavia.uribejeldes@gmail.com>
2022-11-03 12:52:30 -0300

    Avance en js. con el codigo luhn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import validator from './validator.js';
 //validar tarjeta de crédito
-function fValidar tarjeta(){
-var opt = $("lstTipoTarjeta option:selected").val()
-codigo = $("numero_tarjeta").val().replace('-','')
+function "fValidar tarjeta"() {
+var opt = $("lstTipoTarjeta option:selected").val();
+codigo = $("numero_tarjeta").val().replace('-','');
+var msg = "Valor incorrecto";
+VISA = /^4[0-9]{3}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$/;
+MASTERCARD = /^5[1-5][0-9]{2}-?[0-9]{4}-?[0-9]{4}-?[0-9]   {4}$/;
+$("#err_numerotarjeta").html('');
+if(luhn(codigo)) {}
 }
 
 
