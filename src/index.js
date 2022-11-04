5ce4714cc1b9600085fc81501155b15a25f88e51
Flavia Uribe <flavia.uribejeldes@gmail.comgit config --global user.email flavia.uribejeldes@gmail.com>
2022-11-04 19:20:58 -0300

    Realice maquetaje completo con botones funcionales de la tarjeta y escribi el codigo de luhn

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import validator from './validator.js';
-//validar tarjeta de crédito
-function "fValidar tarjeta"() {
-var opt = $("lstTipoTarjeta option:selected").val();
-codigo = $("numero_tarjeta").val().replace('-','');
-var msg = "Valor incorrecto";
-VISA = /^4[0-9]{3}-?[0-9]{4}-?[0-9]{4}-?[0-9]{4}$/;
-MASTERCARD = /^5[1-5][0-9]{2}-?[0-9]{4}-?[0-9]{4}-?[0-9]   {4}$/;
-$("#err_numerotarjeta").html('');
-if(luhn(codigo)) {}
+import validator from './validator.js'
+//Implementación en Javascript del algoritmo de luhn, con funciones de cálculo y validación
+function luhn_checksum(code) {
+ var len = code.length
+ var parity = len % 2
+ var sum = 0
+ for (var i = len-1; i>= 0; i--){
+ var d = parseInt(code.charAt(i))
+ if (i % 2 == parity) { d *= 2 }
+ if (d > 9) { d -= 9}
+ sum += d
 }
+ return sum % 10
+}
+//Devolver el codigo completo (incluido el digito de control), a partir del código especificado
+function luhn_validate(fullcode){
+    return luhn_checksum(fullcode) == 0
+}
+//Devolver respuesta
+function validar_tarjeta(){
+if (sum = 0)
+alert("tarjeta valida")
+}
+function validar_tarjeta(){
+if (sum = "1","2","3","4","5","6","7","8","9")
+alert("tarjeta invalida")
+}
+
 
 
 console.log(validator);
