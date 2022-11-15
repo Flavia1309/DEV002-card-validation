4cd99d59d6d96f65517a37f4cad34d4cf1d03957
Flavia Uribe <flavia.uribejeldes@gmail.comgit config --global user.email flavia.uribejeldes@gmail.com>
2022-11-15 15:01:42 -0300

    Terminando proyecto

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 import validator from './validator.js'
 //Implementación en Javascript del algoritmo de luhn, con funciones de cálculo y validación
-const botonEnviar= document.getElementById("btnEnviar")
-botonEnviar.addEventListener("click", function(e){
-    e.preventDefault();//Implelmentar mi boton de validar
+
+const numeroTarjeta = document.getElementById('creditNumber')
+const numeroMaskify = document.getElementById('numeroMaskify')
+const botonEnviar = document.getElementById("btnEnviar")
+
+botonEnviar.addEventListener("click", function () {
+ // e.preventDefault();//Implelmentar mi boton de validar
+  /*const numeromakify= document.getElementById("numeromaskify");
+      console.log (numeromakify)//implementar que arroje los primeros 12 numeros con el signo #*/
+
+  let numerosTarjetaCredito = document.getElementById("creditNumber").value;//declaro la variable para validar
+
+  //Si la tarjeta es valida, aparecen los numeros ocultos, si no es valida aparece tu tarjeta no es valida
+  if (validator.isValid(numerosTarjetaCredito)) {
+    alert("Tu tarjeta " + validator.maskify(numeroTarjeta.value) + " es válida");
+  } else {
+    alert("Tu tarjeta " + validator.maskify(ccNumber.value) + " es Inválida");
+  }
+  
+  });
+  numeroTarjeta.addEventListener("keyup",function() { //keyup: cada vez que sueltan una tecla, este evento sucede.
+
+  const traerMaskify = validator.maskify(numeroTarjeta.value) //en esta nueva variable, en mi f maskify, entra el valor de ccNumber
+  numeroMaskify.textContent= traerMaskify //el string es devuelto con la propiedad maskify en la interfaz
 });
-const numeromakify= document.getElementById("numeromaskify");
-    console.log (numeromakify)//implementar que arroje los primeros 12 numeros con el signo #
 
-    let creditNumber = document.getElementById(creditNumber).value;//declaro la variable para validar
+  
 
-    //Si la tarjeta es valida, aparecen los numeros ocultos, si no es valida aparece tu tarjeta no es valida
-   if (validator.isValid(creditNumber)) {
-      alert("validar tarjeta numero de tarjeta");  
-    } else {
-      alert("Tu tarjeta es Inválida");
-};
 
-creditNumber.addEventListener("Onkeyup", function() {
 
-const traermaskify = validator.maskify(creditNumber.value)    
-});
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,103 +1,161 @@
 const validator = {
-  isvalid: (creditnumber) => {
-  const length = creditnumber.length;
-  console.log ("btnenviar" + creditnumber)
-  let agregarnumeros = 0 //funcionamiento de mi boton enviar y que acepte solo numeros
-  if(length % 2 == 0){ //le digo que si el digito par multiplicado por 2 es igual a 0
-  
-    for(let e = 0; e < length; e++){//comienza mi funcion 
-      let currentDigit = parseInt(creditnumber[e]);
-      if (e % 2 == 0){//Indico posiciones pares
-  
-        if ((currentDigit *= 2) > 9){//Indico que el numero multiplicado por 2 es mayor a 9
-  
-          let primernumero = parseInt(currentDigit / 10);//Separo los valores y lo indentifica tomando el numero entero
-          let ultimonumero = currentDigit % 10;//Identifica el ultimo numero para el codigo de luhn
+  isValid: (creditNumber) => {
+  const length = creditNumber.length;  //longitud de string
+  let agregarDigitos = 0;
+  //si la cantidad de dígitos, al sacar módulo(%) 2 da como resultado 0
+  if(length % 2 == 0) 
+  {
+      // Comienza al inicio del numero y duplica a partir del primer número
+      for(let i = 0; i < length; i++)
+      {
+          let digitoActual = parseInt(creditNumber[i]);
+          if (i % 2 == 0) //tomar posiciones pares
+          //si el digito actual multiplicado x 2 es mayor que 9
+          {
+              if ((digitoActual *= 2) > 9)
+              {
+                  // Separar los valores de 2 digitos para después sumarlos
+                  let primerNumero = parseInt(digitoActual / 10); //identifica el primer numero de currentDigit tomando el entero, es decir 1 p. ej 16 / 10 = 1.6 = 1
+                  let ultimoNumero = digitoActual % 10; //identifica el ultimo número de current digit con modulo 10 p ej, 16 mod 10 = 6
+                  //confirmar x console log si/en donde se guarda
+                  // suma el primero y último número de currentDigit
+                  digitoActual = primerNumero + ultimoNumero;
+              }
+          }
+          //suma todos los dígitos de la tarjeta
+          agregarDigitos += digitoActual; //consolidar agregarDigitos | Qué pasa con los impares y menores a 9?
+      }
+  }
+    // creo otra constante para alojar la suma de los dígitos, ver si el total sacando su módulo es === a 0,
+    //al ser typeOf retorna un tipo de dato primitivo, si no es true, entonces devuelve false
+    const total = (agregarDigitos % 10) ===0
+    console.log(total, agregarDigitos)
+      return total
+  },
+  // regresa una copia de los números de mi CC. .slice extrae una sección del string y lo devuelve como un nuevo string sin modificar el original. 0 indica el primer elemento, -4 indica desplazamiento desde el final del array, extrae los 4 últimos elementos del array. .replace, reemplaza de forma global por un #
+  maskify: (creditNumber) => {
+  return creditNumber.slice(0, -4).replace(/./g, '#') + creditNumber.slice(-4)
+}
+}
+export default validator;
+//5579100347228064 , 5253460008191610
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const validator = {
+  isValid: (creditNumber) => {
+    let arrayNumber = creditNumber.split("")
+    const length = arrayNumber.length;
+    console.log("btnenviar" + creditNumber)
+    let agregarnumeros = 0 //funcionamiento de mi boton enviar y que acepte solo numeros
+    if (length % 2 == 0) {//Indico posiciones pares
+
+      for (let e = 0; e < length; e++) {//comienza mi funcion 
+      let currentDigit = parseInt(arrayNumber[e]);
+     
+       // console.log("posicion par" + currentDigit)
+        let multDos = currentDigit * 2;
+        console.log("multDos" + multDos)
+        if (multDos > 9) {//Indico que el numero multiplicado por 2 es mayor a 9
+
+          let primernumero = parseInt(multDos / 10);//Separo los valores y lo indentifica tomando el numero entero
+          let ultimonumero = multDos % 10;//Identifica el ultimo numero para el codigo de luhn
           currentDigit = primernumero + ultimonumero//sumar el primer y ultimo numero
+          console.log("mayor a 9 " + currentDigit)
+        } else if (
+          currentDigit = multDos) {
+          // console.log("multDos2 " + currentDigit)
+        } else {
+          currentDigit = currentDigit[e]
+          console.log("impar" + currentDigit)
         }
         agregarnumeros += currentDigit; //sumo todos los digitos de la tarjeta
+        console.log("suma total " + agregarnumeros)
       }
     }
-    const total = (agregarnumeros % 10) ===0
-    console.log(total, agregarnumeros)
-    return total
-  }
-},
-  maskify: (creditnumber) => {
-    return creditnumber.slice(0, -4).replace(/./g, "#") + creditnumber.slice(-4)
-  }
-}
 
-  
-  export default validator;
-  
+    const total = (agregarnumeros % 10) === 0
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//invertir el numero  de tarjeta de credito
-/*invertirDigitos(creditNumber){
-    var invertido= 0
-    var resto= 123456789
-    do{
-    invertido = invertido * 10 + (resto % 10)
-    resto = Math.floor(resto/10)
-    } while (resto > 0)
-    console.log (invertido);
-    document.getElementById(creditNumber).innerHTML = invertido;
-    return invertido
-}
-//Multiplicar los pares de mi numero invertido
-*/
-/*isValid: function(numeroDeTarjeta){
-    console.log(numeroDeTarjeta)
-    let len = numeroDeTarjeta.length
-    let parity = len % 2
-    let sum = 0
-    {
-    let d = parseInt(numeroDeTarjeta.charAt(i))
-    if (i % 2 == parity) { d *= 2 }
-    if (d > 9) { d -= 9}
-    sum += d
-   }
-    if (sum % 10 == 0) {
-    return true  
-    } else  {
-      return false
-    }
+    return total;
   },
-  maskify: function(numeroDeTarjeta){
 
+  maskify: (numeroTarjeta) => {
+    return numeroTarjeta.slice(0, -4).replace(/./g, '#') + numeroTarjeta.slice(-4)
   }
-};*/
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
